b8783ca9d802742dbe8342bfdd3eaa175ec9b1cd
Butter3eer <palcsenge2015@gmail.com>
2023-05-19 11:59:59 +0200

    Initial commit

---
+++ Idozitos-LOIM/Kerdesek.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idozitos_LOIM
+{
+    internal class Kerdesek
+    {
+        private int id;
+        private string kerdes;
+        private string valaszA;
+        private string valaszB;
+        private string valaszC;
+        private string valaszD;
+        private string helyesValasz;
+        private string kateg;
+
+        public Kerdesek(int id, string kerdes, string valaszA, string valaszB, string valaszC, string valaszD, string helyesValasz, string kateg)
+        {
+            this.id = id;
+            this.kerdes = kerdes;
+            this.valaszA = valaszA;
+            this.valaszB = valaszB;
+            this.valaszC = valaszC;
+            this.valaszD = valaszD;
+            this.helyesValasz = helyesValasz;
+            this.kateg = kateg;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Kerdes { get => kerdes; set => kerdes = value; }
+        public string ValaszA { get => valaszA; set => valaszA = value; }
+        public string ValaszB { get => valaszB; set => valaszB = value; }
+        public string ValaszC { get => valaszC; set => valaszC = value; }
+        public string ValaszD { get => valaszD; set => valaszD = value; }
+        public string HelyesValasz { get => helyesValasz; set => helyesValasz = value; }
+        public string Kateg { get => kateg; set => kateg = value; }
+    }
+}
---
+++ Idozitos-LOIM/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Idozitos_LOIM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Kerdesek> kerdesek = new List<Kerdesek>();
+        DispatcherTimer timer = new DispatcherTimer();
+        DispatcherTimer timerSzin = new DispatcherTimer();
+        DispatcherTimer idoBoxTimer = new DispatcherTimer();
+        int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Beolvas();
+            
+            timer.Interval = TimeSpan.FromSeconds(11);
+            timer.Tick += Game;
+
+            idoBoxTimer.Interval = TimeSpan.FromSeconds(1);
+            idoBoxTimer.Tick += Szamlalo;
+            Idomegy.Text = "11";
+        }
+
+        private void Szamlalo(object? sender, EventArgs e)
+        {
+            Szamol();
+        }
+
+        private void Szamol()
+        {  
+            Idomegy.Text = (Convert.ToInt16(Idomegy.Text) - 1).ToString();
+            if (int.Parse(Idomegy.Text) == 0)
+            {
+                idoBoxTimer.Stop();
+                Idomegy.Text = "10";
+            }
+        }
+
+        private void Game(object? sender, EventArgs e)
+        {
+            Szovegek(index);
+            index++;
+            
+        }
+
+        private void Beolvas()
+        {
+            kerdesek.Clear();
+            StreamReader file = new StreamReader("kerdes.txt");
+            while (!file.EndOfStream)
+            {
+                string[] reszek = file.ReadLine().Split(';');
+                kerdesek.Add(new Kerdesek(int.Parse(reszek[0]), reszek[1], reszek[2], reszek[3], reszek[4], reszek[5], reszek[6], reszek[7]));
+            }
+            file.Close();
+            idoBoxTimer.Start();
+            timer.Start();
+        }
+
+        private void Szovegek(int index)
+        {
+            if (index < kerdesek.Count)
+            {
+                Kerdesek item = kerdesek[index];
+
+                Kerdes.Text = item.Kerdes;
+                A.Text = item.ValaszA;
+                B.Text = item.ValaszB;
+                C.Text = item.ValaszC;
+                D.Text = item.ValaszD;
+                A.Background = Brushes.PeachPuff;
+                B.Background = Brushes.PeachPuff;
+                C.Background = Brushes.PeachPuff;
+                D.Background = Brushes.PeachPuff;
+            }
+            else { timer.Stop(); }
+        }
+    }
+}
